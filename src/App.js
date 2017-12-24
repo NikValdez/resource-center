6828d5f65ea38a2fed697adecde9df172bd35558
Nik <nerdvestor@gmail.com>
2017-12-23 20:59:17 -0800

    born

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppBar from 'material-ui/AppBar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <AppBar
+       title="Title"
+       iconClassNameRight="muidocs-icon-navigation-expand-more"
+      />
+      </div>
+    );
+  }
+}
+
+export default App;
