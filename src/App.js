1c08270264dd2f70de8c75bc9304bce60dfdafea
Nik <nerdvestor@gmail.com>
2017-12-24 16:46:43 -0800

    add sidebar funtionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AppBar from 'material-ui/AppBar';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+
+
 
 
 class App extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {open: false};
+ }
   render() {
     return (
       <div className="App">
       <AppBar
        title="Title"
        iconClassNameRight="muidocs-icon-navigation-expand-more"
+       onLeftIconButtonClick={() => this.setState({open: !this.state.open}) }
       />
+      <Drawer
+       open={this.state.open}
+       docked={false}
+       onRequestChange={(open) => this.setState({open})}
+       >
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
